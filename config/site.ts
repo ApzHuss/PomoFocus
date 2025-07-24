d3375452f3178b6b9b172e5bc35b6c55205888a5
Apzhuss <azalaamhuss@gmail.com>
2025-07-24 18:20:39 +0300

    first commit

---
+++ config/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "PomoFocus | Time to Focus!",
+  description: "A simple and effective Pomodoro Timer built with Next.js.",
+  links: {
+    github: "https://github.com/ApzHuss",
+  },
+};
